31d1d16b4e903401ab0322ab54f42ee1d179cc04
valancikas-dot <valancikas@gmail.com>
2026-05-08 18:13:19 +0200

    feat: add Taxlitas card and details page to Flutter app

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"035316565ad77281a75305515e4682e6c4c6f7
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "954715810"
+    serviceWorkerVersion: "3890222908"
   }
 });
